3fc3e7e56e8e3e8f0ea0a63647917eb1932c34b6
Alexander Semenyuk <facemmannn403@gmail.com>
2021-10-18 22:09:01 +0300

    ---

--- app/src/main/java/com/github/justalexandeer/androidstoragesandbox/ui/internal/InternalFragment.kt
+++ app/src/main/java/com/github/justalexandeer/androidstoragesandbox/ui/internal/InternalFragment.kt
@@ -1,11 +1,15 @@
 package com.github.justalexandeer.androidstoragesandbox.ui.internal
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.github.justalexandeer.androidstoragesandbox.databinding.FragmentInternalBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class InternalFragment: Fragment() {
 
@@ -20,4 +24,30 @@ class InternalFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.internalFragmentButtonStart.setOnClickListener {
+            startSomeThing()
+        }
+    }
+
+    fun startSomeThing() {
+        val path = context?.filesDir
+        path?.let {
+            val directory = path.absolutePath.plus("/test")
+            val pathDirectory = File(directory)
+            pathDirectory.mkdir()
+            val newFile = File(directory, "Test")
+            val fileOutputStream = FileOutputStream(newFile)
+            fileOutputStream.use {
+                it.write("firstList".toByteArray())
+            }
+        }
+
+    }
+
+    companion object {
+        private const val TAG = "InternalFragment"
+    }
 }
